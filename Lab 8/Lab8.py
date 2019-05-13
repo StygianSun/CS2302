240c343e4debf8ebef3813ccaa791db011ef446c
StygianSun <38451066+StygianSun@users.noreply.github.com>
2019-05-12 21:07:55 -0600

    Update Lab8.py

--- Lab 8/Lab8.py
+++ Lab 8/Lab8.py
@@ -4,13 +4,14 @@
 @Assignment: Lab 8
 @Instructor: Dr. Olac Fuentes
 @TAs: Anindita Nath and Maliheh Zargaran
-@Date of Last Modification: 5/8/19 @10:43 PM
+@Date of Last Modification: 5/7/19 @9:08 PM
 @Purpose: To design algorithms that 'discover' and test trigonometric identies
 """
 
 import math
 import random
 import numpy as np
+import time
 
 def trigBuild(t):
     """
@@ -86,13 +87,13 @@ def partition(S,n):
     k = sum(S)
     if k%2 != 0:
         print("No partition exists for set: ",S)
-        return False
-    subExists = subset(S,n,k/2)
+        return False, []
+    subExists, s1 = subset(S,n,k/2)
     if subExists:
         print("Partition exists for set: ",S)
     else:
         print("Partition does not exist for set: ",S)
-    return subExists
+    return subExists, s1
 
 def subset(S,n,k):
     """
@@ -107,28 +108,29 @@ def subset(S,n,k):
     If the result of not picking the next number is true, does nothing and
         returns the result of not picking the next number.
     """
-    global s1
     if k == 0:
-        return True
-    if n == 0 and k != 0:
-        return False
-    if S[n-1] > k:
+        return True, []
+    if n<0 or k<0:
+        return False, []
+    se, ss = subset(S,n-1,k-S[n-1])
+    if se:
+        ss.append(n-1)
+        return True, ss
+    else:
         return subset(S,n-1,k)
-    ss1 = subset(S,n-1,k)
-    ss2 = subset(S,n-1,k-S[n-1])
-    if ss2 and not ss1:
-        s1.append(S[n-1])
-        return ss2
-    if ss1:
-        return ss1
 """
 Code for testing of trigonometric identities. Generates a number between -pi and pi,
     then passes that to the trigBuild function, storing the resulting array in
     trig, then passes that onto trigTest
-"""    
-t = random.uniform(-math.pi,math.pi)
-trig = trigBuild(t)
-trigTest(trig)
+""" 
+elapsed = 0
+for i in range(10): 
+    t = random.uniform(-math.pi,math.pi)
+    start = time.time()
+    trig = trigBuild(t)
+    trigTest(trig)
+    elapsed += time.time()-start
+print (elapsed/10)
 
 """
 Code for testing of partition problem. Uses both given sets S so that it can be
@@ -138,29 +140,38 @@ Sorts the given set first.
 Passes the set and length of the set to the partition method:
     Receives the returned boolean of true if a partition is possible and false
         if a partition is not possible.
-If a partition is possible and the half sum of total set is odd, pop the last
-    element added. (This was a strange quirk I noticed about the way I built the subsets.)
-    Build the second subset out of elements that are in set S that are not
-        present in subset 1 (s1).
+If a partition is possible
+    Builds the two subsets by appending the values of the indices of S found in
+        the list of indices returned by partition into s1, and those indices
+        not found in the list to s2.
     Prints subset 1 (s1).
     Prints subset 2 (s2).
 
 Then tests against a set that does not have a partition possible.
 """
-S = [2,4,5,9,12]
-S.sort()
-s1 = []
-sub = partition(S,len(S))
-if sub:
-    if (sum(S)/2) != 1:
-        s1.pop()
-    s2 = []
-    for i in S:
-        if i not in s1:
-            s2.append(i)
-    print(s1)
-    print(s2)
+elapsed = 0
+n = 5
+S=np.zeros(n,dtype=int)
+for j in range(10):
+    for i in range(n):
+        S[i] = random.randint(0,n)
+    S.sort()
+    start = time.time()
+    sub, ss = partition(S,len(S))
+    if sub:
+        s1 = []
+        s2 = []
+        for i in range(len(S)):
+            if i in ss:
+                s1.append(S[i])
+            else:
+                s2.append(S[i])
+        print(s1)
+        print(s2)
+    elapsed += time.time()-start
     
+print(elapsed/10)
+"""   
 S = [2,4,5,9,13]
 S.sort()
 s1 = []
@@ -173,4 +184,5 @@ if sub:
         if i not in s1:
             s2.append(i)
     print(s1)
-    print(s2)  
+    print(s2)
+"""
